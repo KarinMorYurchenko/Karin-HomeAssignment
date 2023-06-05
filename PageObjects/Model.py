b3eeb60147543f2eee956c5de56ab6cf6d486a1d
KarinYurchenko <karin.yurchenko@gmail.com>
2023-06-05 17:15:23 +0300

    commands

--- PageObjects/Model.py
+++ PageObjects/Model.py
@@ -12,19 +12,19 @@ class ModelsPage:
 
     @allure.step("Model name element")
     def model_name_element(self):
-        WebDriverWait(self.driver, 3).until(
+        WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '#cell-cell_0_model > div > div > a')))
         return self.driver.find_element(By.CSS_SELECTOR, '#cell-cell_0_model > div > div > a')
 
     @allure.step("Supported Commands")
     def supported_commands_element(self):
-        WebDriverWait(self.driver, 3).until(
+        WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="location-tab-commands"]')))
         return self.driver.find_element(By.CSS_SELECTOR, '[data-testid="location-tab-commands"]')
 
     @allure.step("Add Command Button Element")
     def add_command_element(self):
-        WebDriverWait(self.driver, 3).until(
+        WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="add-command-btn"]')))
         return self.driver.find_element(By.CSS_SELECTOR, '[data-testid="add-command-btn"]')
 
